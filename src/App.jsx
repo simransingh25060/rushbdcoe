01edbe7ef20fd0ca81b7271b2b87871268fec171
Pranay623 <srivastavapranay623@gmail.com>
2024-11-30 18:45:55 +0530

    home page done and routing set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './components/home/home'
+import Leaderboard from './components/leaderboard/leaderboard'
+import Timer from './components/timer/timer'
 import './App.css'
+import {Route,Routes} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +13,11 @@ function App() {
   return (
     <>
       <div>
-        <Home/>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/leaderboard" element={<Leaderboard/>} />
+      <Route path="/timer" element={<Timer/>} />
+    </Routes>
       </div>
         
     </>
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import bar from '../home/assests/bar.svg'
+import flag from '../home/assests/lead.png'
 
 const home = () => {
   return (
     <div className='bg-[#171921] w-full h-screen'>
-        <span className='text-white'>home</span>
+        <div className='flex items-baseline justify-center gap-4 pt-[4rem]'>
+        <img src={flag} className='w-[5vw] h-[5vh]'/>
+        </div>
+        <div className='flex items-baseline justify-center gap-4'>
+        {/* <img src={flag} className='w-[3vw] h-[4vh]'/> */}
+            <span className='text-white font-Belleza text-[3rem]'>LEADERBOARD</span>
+        </div>
+        <div className='flex items-baseline justify-center gap-4 pt-[4rem]'>
+            <img src='https://ucarecdn.com/d7633d0f-f620-4fb1-8c0f-51041e187436/' className='w-[3vw] h-[4vh]'/>
+            <span className='text-white font-Belleza text-[3rem]'>BIG DATA CENTRE OF EXCELLENCE</span>
+        </div>
+        <div className='flex justify-center pt-3'>
+            <img src="https://ucarecdn.com/2e717701-3aae-4a2f-b3b8-3c5fe055b86a/RUSHHOUR20.png" className='w-[65rem]'/>
+        </div>
+        <div className='flex justify-center pt-3'>
+            <span className='text-white font-Gabarito text-[2rem]'>Race the clock !</span>
+        </div>
+        <div className='flex justify-between mr-[2rem] ml-[2rem] pt-14'>
+            <img src={bar} className='w-[12rem] transform scale-x-[-1]'/>
+            <img src={bar} className='w-[12rem]'/>
+        </div>
+        
+        
         </div>
   )
 }
