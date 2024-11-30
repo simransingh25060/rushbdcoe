59af32f508954873277c03fdc0a0882a04df920c
Pranay623 <srivastavapranay623@gmail.com>
2024-11-30 16:24:02 +0530

    Leaderboard Structure

---
+++ src/components/home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='bg-[#171921] w-full h-screen'>
+        <span className='text-white'>home</span>
+        </div>
+  )
+}
+
+export default home
---
+++ src/components/timer/timer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const timer = () => {
+  return (
+    <div>timer</div>
+  )
+}
+
+export default timer
