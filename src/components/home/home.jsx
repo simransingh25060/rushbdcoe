aedc796c9157bfac7b86b4a1b423960a59fcf5fa
Pranay623 <srivastavapranay623@gmail.com>
2024-11-30 19:01:36 +0530

    changes in home page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,7 +4,7 @@ import flag from '../home/assests/lead.png'
 
 const home = () => {
   return (
-    <div className='bg-[#171921] w-full h-screen'>
+    <div className='bg-[#171921] w-full h-screen bg-cover'>
         <div className='flex items-baseline justify-center gap-4 pt-[4rem]'>
         <img src={flag} className='w-[5vw] h-[5vh]'/>
         </div>
--- src/components/leaderboard/leaderboard.jsx
+++ src/components/leaderboard/leaderboard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import bar from '../home/assests/bar.svg'
 
 const leaderboard = () => {
   return (
-    <div>leaderboard</div>
+    <div className='bg-[#171921] w-full h-screen'>
+        <div className='flex justify-between mr-[2rem] ml-[2rem] pt-14'>
+            <img src={bar} className='w-[12rem] transform scale-x-[-1]'/>
+            <img src={bar} className='w-[12rem]'/>
+        </div>
+    </div>
   )
 }
 
