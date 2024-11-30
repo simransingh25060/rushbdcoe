45dab7adf134f076ea73ea6f6d899243e01f9b82
Pranay623 <srivastavapranay623@gmail.com>
2024-11-30 19:06:56 +0530

    home changes in responsiveness

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,15 +4,15 @@ import flag from '../home/assests/lead.png'
 
 const home = () => {
   return (
-    <div className='bg-[#171921] w-full h-screen bg-cover'>
-        <div className='flex items-baseline justify-center gap-4 pt-[4rem]'>
+    <div className='bg-[#171921] w-full h-screen'>
+        <div className='flex items-baseline justify-center gap-4 pt-[3rem]'>
         <img src={flag} className='w-[5vw] h-[5vh]'/>
         </div>
         <div className='flex items-baseline justify-center gap-4'>
         {/* <img src={flag} className='w-[3vw] h-[4vh]'/> */}
             <span className='text-white font-Belleza text-[3rem]'>LEADERBOARD</span>
         </div>
-        <div className='flex items-baseline justify-center gap-4 pt-[4rem]'>
+        <div className='flex items-baseline justify-center gap-4 pt-[2rem]'>
             <img src='https://ucarecdn.com/d7633d0f-f620-4fb1-8c0f-51041e187436/' className='w-[3vw] h-[4vh]'/>
             <span className='text-white font-Belleza text-[3rem]'>BIG DATA CENTRE OF EXCELLENCE</span>
         </div>
@@ -22,7 +22,7 @@ const home = () => {
         <div className='flex justify-center pt-3'>
             <span className='text-white font-Gabarito text-[2rem]'>Race the clock !</span>
         </div>
-        <div className='flex justify-between mr-[2rem] ml-[2rem] pt-14'>
+        <div className='flex justify-between mr-[2rem] ml-[2rem] pt-9'>
             <img src={bar} className='w-[12rem] transform scale-x-[-1]'/>
             <img src={bar} className='w-[12rem]'/>
         </div>
